d38cb4c4868a4af87cf4618879298ee31548cd00
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-03-28 17:44:50 +0100

    Implement insert and find methods

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -37,14 +37,55 @@ class Tree
         pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
 
     end
+
+    def insert(number, node = @root)
+
+        if number > node.data && node.right_child == nil
+            node.right_child = Node.new(number)
+        elsif number < node.data && node.left_child == nil
+            node.left_child = Node.new(number)
+        elsif number > node.data
+            insert(number, node.right_child)
+        elsif number < node.data
+            insert(number, node.left_child)
+        end
+    end
+
+    def delete(value)
+        
+        # you’ll have to deal with several cases for delete such as when a node has children or not
+
+        # if !find(value), non existent node with that value
+
+        # if it doesn't have children, just set previous node's corresponding child to nil
+
+        # if it only has one child, replace node with child
+
+        # if two children, replace node with inorder values and reinsert them?
+    end
+
+    def find(value, node = @root)
+
+       if value == node.data
+            node
+       elsif value < node.data
+            find(value, node.left_child) if node.left_child
+       elsif value > node.data
+            find(value, node.right_child) if node.right_child
+       end
+        
+    end
 end
 
 bst = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
-
-p srt_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324].uniq.sort
-p srt_array[srt_array.length/ 2]
-
 p bst
 
 bst.pretty_print
+
+p bst.insert(6)
+p bst.insert(24)
+p bst.insert(0)
+
+bst.pretty_print
+p bst.find(6)
